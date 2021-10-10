45fd9d3629d90f6c63158bb41058fb99fc6527bb
Abdessamad DERRAZ <derraz.abdessamad@gmail.com>
2021-10-10 22:40:41 +0200

    First Commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Avis Restau
+
+![Logo Avis Restau](data/images/logo.png)
+
+Avis Restau souhaite améliorer sa plateforme avec une **nouvelle fonctionnalité de collaboration** pour ses utilisateurs où ils pourront par exemple poster des avis et des photos sur leur restaurant préféré.
+
+C’est aussi un moyen de mieux comprendre les avis postés par les utilisateurs.
+
+Ider, le CTO de l’entreprise, à des souhaits que nous pouvons diviser en deux parties, textuel et visuel :
+- Ses besoins textuels sont la détection des sujets d’insatisfaction présents dans les commentaires postés sur la plateforme.
+- Ses besoins visuels sont la labellisation automatique des photos postées sur la plateforme. Par exemple, identifier les photos relatives à la nourriture, au décor dans le restaurant ou à l’extérieur du restaurant.
+
+Cependant, Avis Restau manque de données. Nous allons donc utiliser la base de données de Yelp pour réaliser une preuve de concept.
+
+Donc pour répondre à sa demande le projet sera divisé ainsi :
+1. Récolte des données sur Yelp
+2. Analyse textuelle
+3. Analyse visuelle
---
+++ functions.py
@@ -0,0 +1,63 @@
+import os
+import pandas as pd
+# import spacy
+# from joblib import Parallel, delayed
+
+# nlp = spacy.load('fr_core_news_lg')
+# nlp.add_pipe('sentencizer')
+
+def appendDFToCSV_void(df, csvFilePath, sep=","):
+    if not os.path.isfile(csvFilePath):
+        df.to_csv(csvFilePath, mode='a', index=False, sep=sep)
+    elif len(df.columns) != len(pd.read_csv(csvFilePath, nrows=1, sep=sep).columns):
+        raise Exception("Les colonnes ne correspondent pas ! Le dataframe a " + str(len(df.columns)) +
+                        " colonnes. Le fichier CSV a " + str(len(pd.read_csv(csvFilePath, nrows=1, sep=sep).columns)) + " colonnes.")
+    elif not (df.columns == pd.read_csv(csvFilePath, nrows=1, sep=sep).columns).all():
+        raise Exception(
+            "Les colonnes et l'ordre des colonnes du dataframe et du fichier csv ne correspondent pas !")
+    else:
+        df.to_csv(csvFilePath, mode='a', index=False, sep=sep, header=False)
+
+
+# def chunker(iterable, total_length, chunksize):
+#     return (iterable[pos: pos + chunksize] for pos in range(0, total_length, chunksize))
+
+# def flatten(list_of_lists):
+#     "Flatten a list of lists to a combined list"
+#     return [item for sublist in list_of_lists for item in sublist]
+
+# def lemmatize_pipe(doc):
+#     lemma_list = [str(tok.lemma_).lower() for tok in doc
+#                   if tok.is_alpha and tok.text.lower() not in stopwords] 
+#     return lemma_list
+
+# def process_chunk(texts):
+#     preproc_pipe = []
+#     for doc in nlp.pipe(texts, batch_size=20):
+#         preproc_pipe.append(lemmatize_pipe(doc))
+#     return preproc_pipe
+
+# def preprocess_parallel(df_preproc, texts, chunksize=100):
+#     executor = Parallel(n_jobs=7, backend='multiprocessing', prefer="processes")
+#     do = delayed(process_chunk)
+#     tasks = (do(chunk) for chunk in chunker(texts, len(df_preproc), chunksize=chunksize))
+#     result = executor(tasks)
+#     return flatten(result)
+
+# def preprocess_pipe(texts):
+#     preproc_pipe = []
+#     for doc in nlp.pipe(texts, batch_size=20):
+#         preproc_pipe.append(lemmatize_pipe(doc))
+#     return preproc_pipe
+
+# def get_stopwords():
+#     "Renvoie un ensemble de stopwords lus à partir d'un fichier."
+#     with open(stopwordfile) as f:
+#         stopwords = []
+#         for line in f:
+#             stopwords.append(line.strip("\n"))
+#     # Convertir en un ensemble pour la performance
+#     stopwords_set = set(stopwords)
+#     return stopwords_set
+
+# stopwords = get_stopwords()
